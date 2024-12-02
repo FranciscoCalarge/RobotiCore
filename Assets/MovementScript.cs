e0d4cede66b08f2c9fe1da9160034c66e84ba373
Francisco José Costa Calarge <54690356+FranciscoCalarge@users.noreply.github.com>
2024-12-01 20:35:35 -0400

    fixed movement commit

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -4,6 +4,7 @@ public class MovementScript : MonoBehaviour
 {
     public Rigidbody body;
     public Transform camera;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,10 +14,9 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 inputVec3 = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        body.MovePosition(transform.position+inputVec3/10);
-        Vector3 lookAtVec3 = camera.transform.forward;
-        lookAtVec3.Scale(new Vector3(1, 0, 1));
-        body.MoveRotation(Quaternion.FromToRotation(transform.forward, lookAtVec3 ));
+        Vector3 lookOffset = transform.position-camera.position;
+        transform.forward = new Vector3(lookOffset.x,0,lookOffset.z);
+        body.MovePosition(transform.position+camera.right*Input.GetAxis("Horizontal")/10+camera.forward*Input.GetAxis("Vertical")/10);
+
     }
 }
